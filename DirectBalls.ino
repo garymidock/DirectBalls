9afcfd7ecfd55e358cd60ab8b3fb7b1ae6a80c22
garymidock <gary.midock@gmail.com>
2016-03-18 16:21:18 -0400

    Add DirectBalls.ino
    This is the code

---
+++ DirectBalls.ino
@@ -0,0 +1,32 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //release balls
+
+  //flash lights
+
+  //display final light pattern
+  int lightKey = random(3);
+  switch (lightKey)
+  {
+    case 1:
+    Serial.println("final light pattern: ");
+    break;
+  }
+  
+  
+
+  //read direction
+
+  //set gates
+
+  //grade effort
+
+  
+
+}
