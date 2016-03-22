d56f99d62b60568d9f88358d0a7fce33eda22ad9
garymidock <gary.midock@gmail.com>
2016-03-22 00:51:50 -0400

    Added code for states 1, 2, and 3.
    Checking placement of break statement.
    Now lights lights flash in state 2 until millis() > timeFor StateChange, at which time red light on pin13 stops flashing and takes on a solid off or on state.

--- DirectBalls.ino
+++ DirectBalls.ino
@@ -5,71 +5,113 @@ const int greenLightPin = 12; //arduino digital pin 12 controls green light
 const int blueLightPin  = 11; //arduino digital pin 13 controls blue light
 
 // Variables will change:
-int redLedState   = HIGH;         // the current state of the red light output pin
-int greenLedState = HIGH;         // the current state of the green light output pin
-int blueLedState  = HIGH;         // the current state of the blue light output pin
-int buttonState;             // the current reading from the input pin
-int lastButtonState = LOW;   // the previous reading from the input pin
+int redLedState          = HIGH;         // the current state of the red light output pin
+int greenLedState        = HIGH;         // the current state of the green light output pin
+int blueLedState         = HIGH;         // the current state of the blue light output pin
+int stateNum             = 1;            // contains the number of the state being executed
+int lastButtonState      = LOW;          // the previous reading from the input pin
+int ballIsInPosition     = 0;            // when true, display final light pattern
 
 // the following variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
-long lastRedLedChangeTime   = 0;  // the last time the output pin was toggled
-long lastGreenLedChangeTime = 0;  // the last time the output pin was toggled
-long lastBlueLedChangeTime  = 0;  // the last time the output pin was toggled
+long lastRedLedChangeTime   = 0;     // the last time the output pin was toggled
+long lastGreenLedChangeTime = 0;     // the last time the output pin was toggled
+long lastBlueLedChangeTime  = 0;     // the last time the output pin was toggled
 long redLightChangeDelay    = 50;    // the debounce time; increase if the output flickers
 long greenLightChangeDelay  = 50;    // the debounce time; increase if the output flickers
 long blueLightChangeDelay   = 50;    // the debounce time; increase if the output flickers
 
+//testing variables
+int counter = 0;
+long timeForStateChange = 5000;     //stay in a particular state for this time only
+
 void setup() {
   // put your setup code here, to run once:
   // initialize digital light pins as outputs.
+  Serial.begin(9600);
   pinMode(redLightPin, OUTPUT);
   pinMode(greenLightPin, OUTPUT);
   pinMode(blueLightPin, OUTPUT);
+
+  Serial.println("end of setup");
+  Serial.print("timeForStateChange: " );
+  Serial.println(timeForStateChange);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  //release balls
-
-  //flash lights
-  if ((millis() - lastRedLedChangeTime) > redLightChangeDelay) {
-    // whatever the reading is at, it's been there for longer
-    // than the debounce delay, so take it as the actual current state:
-    redLedState = !redLedState;
-    lastRedLedChangeTime = millis();
-  }
-
-  digitalWrite(redLightPin, redLedState);
-
-  //display final light pattern
-  int lightKey = random(2);
-  switch (lightKey)
+  switch (stateNum)
   {
+
+    //release balls
     case 1:
-//    Serial.println("displaying final light pattern: ");
-    break;
+      stateNum = 2; //balls have been released so flash lights
+      break;
 
-      //read direction
-  {
-  case 2:
-  Serial.println("reading direction given by child to kinect: ");
-  break;
-    
+    //flash lights
+    case 2:
+      if ((millis() - lastRedLedChangeTime) > redLightChangeDelay) {
+        // whatever the reading is at, it's been there for longer
+        // than the debounce delay, so take it as the actual current state:
+        redLedState = !redLedState;
+        lastRedLedChangeTime = millis();
+      }
+
+      if ((millis() - lastGreenLedChangeTime) > greenLightChangeDelay) {
+        // whatever the reading is at, it's been there for longer
+        // than the debounce delay, so take it as the actual current state:
+        greenLedState = !greenLedState;
+        lastGreenLedChangeTime = millis();
+      }
+
+      if ((millis() - lastBlueLedChangeTime) > blueLightChangeDelay) {
+        // whatever the reading is at, it's been there for longer
+        // than the debounce delay, so take it as the actual current state:
+        blueLedState = !blueLedState;
+        lastBlueLedChangeTime = millis();
+      }
+      //      int timeDiff =0;
+      //      Serial.print("timeDiff: ");
+      //      Serial.println(timeDiff);
+
+      //      if ((millis() - timeForStateChange) < 0)
+      //      {
+      //        Serial.println("setting ballIsInPostion to 1");
+      //        ballIsInPosition = 1;
+      //      }
+      if (millis() > timeForStateChange)
+      {
+        stateNum = 3;
+
+      }
+      if (ballIsInPosition == 1)
+      {
+        //        stateNum = 3; //lights have been flashing and ball is in position for final light pattern
+        //        break;
+      }
+
+      break;
+
+    //display final light pattern
+    case 3:
+      digitalWrite(redLightPin, HIGH);
+      redLedState = HIGH;
+
+      //set gates
+
+      //grade effort
+
   }
+  digitalWrite(redLightPin, redLedState);
+  digitalWrite(greenLightPin, greenLedState);
+  digitalWrite(blueLightPin, blueLedState);
+  Serial.print("millis(): ");
+  Serial.println(millis());
+  Serial.print("stateNum: ");
+  Serial.println(stateNum);
 
-  //set gates
-
-  //grade effort
-
-  }
-  
-  
-  
-
-
-  
 
 }
+
