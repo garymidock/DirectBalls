3ec8d8589ebb76c9924c3f4f9469888ac0753217
garymidock <gary.midock@gmail.com>
2016-03-28 21:37:45 -0400

    Added digital inputs to switch states.
    Added digital inputs to switch to state 2  and state 3

--- DirectBalls.ino
+++ DirectBalls.ino
@@ -4,7 +4,9 @@ const int redLightPin    = 5; //arduino digital pin 5 controls red light
 const int greenLightPin  = 6; //arduino digital pin 6 controls green light
 const int blueLightPin   = 7; //arduino digital pin 7 controls blue light
 
-const int stateSwitchPin = 4; //pin 4 has PB that increments stateNum
+const int ballReleasedPin    = 22; //pin 2 is 1 when ball is released
+const int Pin3               = 3;  //WHY IS PIN 3 ALWAYS 1? It is always 1 do to the motor driver on the motor control shield
+const int ballAtGatePin      = 24; //pin 4 is 1 when ball is at gate
 
 // Variables will change:
 int redLedState          = HIGH;         // the current state of the red light output pin
@@ -12,8 +14,10 @@ int greenLedState        = HIGH;         // the current state of the green light
 int blueLedState         = HIGH;         // the current state of the blue light output pin
 int stateNum             = 1;            // contains the number of the state being executed
 int lastButtonState      = LOW;          // the previous reading from the input pin
-int ballIsInPosition     = 0;            // when true, display final light pattern
+int ballAtGate           = 0;            // when true, display final light pattern
+int ballReleased         = 0;            // when true, ball has been released, flash lights
 int switchState          = 0;            // 1 = switch state
+int valueOfPin3          = 0;
 // the following variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
 long lastRedLedChangeTime   = 0;     // the last time the output pin was toggled
@@ -35,6 +39,11 @@ void setup() {
   pinMode(greenLightPin, OUTPUT);
   pinMode(blueLightPin, OUTPUT);
 
+  pinMode(ballReleased, INPUT);
+  pinMode(ballAtGate, INPUT);
+
+  pinMode(Pin3, INPUT);
+
   Serial.println("end of setup");
   Serial.print("timeForStateChange: " );
   Serial.println(timeForStateChange);
@@ -43,15 +52,30 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  ballReleased = digitalRead(ballReleasedPin);
+  ballAtGate   = digitalRead(ballAtGatePin);
 
-  switchState = digitalRead(stateSwitchPin);
+  
+
+  Serial.print("ballReleased: ");
+  Serial.println(ballReleased);
+  Serial.print("ballAtGate: ");
+  Serial.println(ballAtGate);
+
+  int valueOfPin3 = digitalRead(Pin3);
+  Serial.print("valueOfPin3: ");
+  Serial.println(valueOfPin3);
+  
 
   switch (stateNum)
   {
 
     //release balls
     case 1:
+    if (ballReleased == 1)
+    {
       stateNum = 2; //balls have been released so flash lights
+    }
       break;
 
     //flash lights
@@ -76,27 +100,9 @@ void loop() {
         blueLedState = !blueLedState;
         lastBlueLedChangeTime = millis();
       }
-      //      int timeDiff =0;
-      //      Serial.print("timeDiff: ");
-      //      Serial.println(timeDiff);
-
-      //      if ((millis() - timeForStateChange) < 0)
-      //      {
-      //        Serial.println("setting ballIsInPostion to 1");
-      //        ballIsInPosition = 1;
-      //      }
-//      if (millis() > timeForStateChange)
-//      {
-//        stateNum = 3;
-//
-//      }
-      if (switchState == 1)
+      if (ballAtGate == 1)
       {
-        stateNum += 1;
-      }
-      if (ballIsInPosition == 1)
-      {
-        //        stateNum = 3; //lights have been flashing and ball is in position for final light pattern
+                stateNum += 1; //lights have been flashing and ball is in position for final light pattern
         //        break;
       }
 
@@ -119,8 +125,6 @@ void loop() {
   digitalWrite(redLightPin, redLedState);
   digitalWrite(greenLightPin, greenLedState);
   digitalWrite(blueLightPin, blueLedState);
-  Serial.print("millis(): ");
-  Serial.println(millis());
   Serial.print("stateNum: ");
   Serial.println(stateNum);
 
